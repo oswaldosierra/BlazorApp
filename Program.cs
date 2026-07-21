bab3564fc3d40c1bde81bd7c2f174c4b4c4c2ac4
Oswaldo Sierra <oswaldox199@gmail.com>
2026-07-21 10:47:49 -0500

    Agregar configuración de apiUrl en Program.cs y actualizar appsettings.json

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+var apiUrl = builder.Configuration.GetValue<string>("apiUrl");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiUrl) });
 
 await builder.Build().RunAsync();
